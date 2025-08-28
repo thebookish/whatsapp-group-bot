b0400473b0e1bdbd7b5fb3826160354ab46747fb
thebookish <abulhasannabil@gmail.com>
2025-08-28 12:46:14 +0600

    add data file

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL || 'https://jlznlwkluocqjnepxwbv.supabase.co';
 const supabaseKey = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impsem5sd2tsdW9jcWpuZXB4d2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3MDA3MDAsImV4cCI6MjA3MTI3NjcwMH0.8gFwwwcV9w2Pcs-QObN2uyuxnf9lGjzhRotR56BMTwo';
 const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY || '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';
-
+const DATA_FILE = process.env.PROVIDERS_DATA_PATH || path.join(process.cwd(), 'providers_with_courses.json');
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -12,4 +12,5 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = {
 supabase,
 OPENROUTER_API_KEY,
+DATA_FILE
 };
